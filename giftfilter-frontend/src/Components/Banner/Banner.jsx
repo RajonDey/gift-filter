0555cd9c20ce0923fa5d858bf43ff05d743d7bc1
ReshadSadik <reshadsadik@gmail.com>
2023-04-02 02:07:33 +0600

    second blog published

--- giftfilter-frontend/src/Components/Banner/Banner.jsx
+++ giftfilter-frontend/src/Components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
         Find the <span>perfect gift</span> for <span>every occasion.</span>
       </h1>
       <h3>Create unforgettable moments with our gift collection.</h3>
-      <FilterSection />
+      {/* <FilterSection /> */}
     </section>
   );
 };
